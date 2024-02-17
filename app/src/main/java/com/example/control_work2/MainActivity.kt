9e5cef50698d51ab60073cfcbff870f0055cec7f
Oreoff <maximmelnik37@gmail.com>
2024-02-17 13:30:13 +0200

    added text and design

--- app/src/main/java/com/example/control_work2/MainActivity.kt
+++ app/src/main/java/com/example/control_work2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.foundation.border
+import androidx.compose.foundation.focusable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -25,6 +27,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.control_work2.ui.theme.Control_Work2Theme
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,13 +63,21 @@ fun Greeting(modifier: Modifier = Modifier
         5 -> R.drawable.image_5
         else -> R.drawable.ic_launcher_background
     }
-    val text = when (result) {
+    val desc = when (result) {
         1 -> "Image 1"
         2 -> "Image 2"
         3 -> "Image 3"
         4 -> "Image 4"
         5 -> "Image 5"
         else -> "Error:Image not found"
+    }
+    val author = when (result) {
+        1 -> "Hanz Zimmer"
+        2 -> "Maxym Melnyk"
+        3 -> "Vincent Van Gog"
+        4 -> "Michelangelo"
+        5 -> "Rick Astley"
+        else -> "Developer is retard, sorry"
     }
     fun ImageScrollerForward():Int
     {
@@ -82,20 +95,24 @@ fun Greeting(modifier: Modifier = Modifier
     }
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
 
     )
         {
             Box(
                 modifier = Modifier
 
-                    .border(1.dp, Color.Gray)
+
             ) {
 
                 Column(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.height(500.dp).fillMaxWidth()
+                    modifier = Modifier
+                        .height(500.dp)
+                        .fillMaxWidth()
 
                 ) {
                     Image(
@@ -103,6 +120,13 @@ fun Greeting(modifier: Modifier = Modifier
                         contentDescription = result.toString()
                     )
                 }
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                .background(Color.LightGray)
+                .fillMaxWidth()) {
+                Text(text = desc, fontSize = 36.sp)
+                Text(text = author, fontSize = 14.sp)
             }
           Row(horizontalArrangement = Arrangement.SpaceBetween,
               modifier = Modifier.fillMaxWidth()
