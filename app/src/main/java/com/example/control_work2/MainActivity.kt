ece8ed0a174d5a3c275348da68b1da293b7ec7f2
Oreoff <maximmelnik37@gmail.com>
2024-02-17 12:35:10 +0200

    Made scroller & buttons & added images

--- app/src/main/java/com/example/control_work2/MainActivity.kt
+++ app/src/main/java/com/example/control_work2/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.control_work2
-
+import androidx.compose.foundation.Image
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.control_work2.ui.theme.Control_Work2Theme
-
+import androidx.compose.material3.Button
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +39,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Greeting(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center))
+{ var result by remember { mutableStateOf(1)
+    }
 
+    val imageResource = when (result) {
+        1 -> R.drawable.image_1
+        2 -> R.drawable.image_2
+        3 -> R.drawable.image_3
+        4 -> R.drawable.image_4
+        5 -> R.drawable.image_5
+        else -> R.drawable.ic_launcher_background
+    }
+    fun ImageScrollerForward():Int
+    {
+        if(result == 5)result = 1
+        else result++;
+
+        return 0;
+    }
+    fun ImageScrollerBackward():Int
+    {
+        if(result == 1)result = 5
+        else result--;
+
+        return 0;
+    }
+    Column (modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,)
+        {
+Image(
+    painter = painterResource(id = imageResource),
+    contentDescription =  result.toString()
+)
+            Button(
+                onClick = {ImageScrollerBackward()})
+            {
+                Text("Previous")
+            }
+            Button(
+                onClick = {ImageScrollerForward()})
+                {
+                    Text("Next")
+                }
+        }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Control_Work2Theme {
-        Greeting("Android")
+        Greeting()
     }
 }
